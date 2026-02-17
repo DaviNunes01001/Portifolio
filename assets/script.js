13030cdc209d24f1f099501d4a68a2468cdc5f48
hakizao6074 <davizinhodjar@gmail.com>
2026-02-17 08:47:20 -0300

    Add modo-escuro(1).png to assets/svg directory

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const bntntema = document.getElementById("bntema");
+
+bntntema.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark");
+
+  const isDark = document.body.classList.contains("dark");
+
+  if (isDark) {
+    bntntema.src = "../assets/svg/modo-escuro(1).png";
+  } else {
+    bntntema.src = "../assets/svg/modo-claro.png";
+  }
+
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+
+});
+
+window.addEventListener("load", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    bntntema.src = "../assets/svg/modo-escuro(1).png";
+  } else {
+    bntntema.src = "../assets/svg/modo-claro.png";
+  }
+});
