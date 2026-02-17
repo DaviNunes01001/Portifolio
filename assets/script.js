67b3de64e327898a622984088d9c29345faa8947
hakizao6074 <davizinhodjar@gmail.com>
2026-02-17 08:49:31 -0300

    fix: corrigir caminhos das imagens do tema

--- assets/script.js
+++ assets/script.js
@@ -7,9 +7,9 @@ bntntema.addEventListener("click", () => {
   const isDark = document.body.classList.contains("dark");
 
   if (isDark) {
-    bntntema.src = "../assets/svg/modo-escuro(1).png";
+    bntntema.src = "assets/svg/modo-escuro(1).png";
   } else {
-    bntntema.src = "../assets/svg/modo-claro.png";
+    bntntema.src = "assets/svg/modo-claro.png";
   }
 
   localStorage.setItem("theme", isDark ? "dark" : "light");
